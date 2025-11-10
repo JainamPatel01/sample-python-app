3d0be7bfdc35a499ab557789e43eb54b3ef43909
JainamPatel01 <lucifer10829@gmail.com>
2025-11-10 15:12:17 +0530

    Create output.tf

---
+++ infra/output.tf
@@ -0,0 +1,12 @@
+output "alb_dns" {
+  description = "ALB DNS name"
+  value       = aws_lb.alb.dns_name
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "ecs_cluster" {
+  value = aws_ecs_cluster.cluster.id
+}
