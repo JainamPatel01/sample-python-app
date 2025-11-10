0a21f5d613420f536831975c1eeb2dcd2e964414
JainamPatel01 <lucifer10829@gmail.com>
2025-11-10 15:05:49 +0530

    Add files via upload

---
+++ ecr.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.project_name}"
+  image_scanning_configuration { scan_on_push = true }
+  image_tag_mutability = "MUTABLE"
+  tags = { Name = var.project_name }
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app.repository_url
+}
