bed7e206c6ffff39035862587c3bd965465c91d7
JainamPatel01 <lucifer10829@gmail.com>
2025-11-10 16:01:36 +0530

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -3,9 +3,6 @@ output "alb_dns" {
   value       = aws_lb.alb.dns_name
 }
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app.repository_url
-}
 
 output "ecs_cluster" {
   value = aws_ecs_cluster.cluster.id
